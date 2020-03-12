fe730420d63600c0e336dca6665178ffcb31baec
asomeJay <museum114@naver.com>
2020-03-12 23:51:08 +0900

    다리 만들기 2
    최적화를 좀 시켰다.

--- Acm_icpc/C,C++/10000/boj17472.cpp
+++ Acm_icpc/C,C++/10000/boj17472.cpp
@@ -41,7 +41,7 @@ int ROW, COL, last_island, mst_weight;
 void init();
 void input();
 
-// Island Work : Identificaion, Seiza Connection Relation
+// Island Work : Identificaion, Seizㄷ Connection Relation
 int island_identification();
 void identification(int, int, int& island_num);
 bool bridge_connection(int, int);
@@ -133,6 +133,8 @@ bool bridge_connection(int one, int two) {
 	for (i = 0; i < cord[one].size(); i++) {
 		for (j = 0; j < cord[two].size(); j++) {
 			if (cord[one][i].first == cord[two][j].first) { // Row가 같은 케이스
+				//one 과 two 중에 누가 더 오른쪽에 있는지는 결정해줘야지 
+				// k가 왼쪽, t가 오른쪽에 있고 k ~ t 까지 진행하면서 다른 애있는지 체크할거야
 				if (cord[one][i].second > cord[two][j].second) {
 					k = cord[two][j].second;
 					t = cord[one][i].second;
@@ -141,11 +143,13 @@ bool bridge_connection(int one, int two) {
 					k = cord[one][i].second;
 					t = cord[two][j].second;
 				}
-				for (p = k + 1; p < t; p++) {
+				for (p = k + 1; p < t; p++) { // 진행하다가 다른 애들 만나면 여긴 도로 못놓음
 					if (map[cord[one][i].first][p] != SEA)
 						break;
 				}
-				if (p == t) {
+
+				if (p == t) { // 다른애가 없네? 그러면 도로 놓을 수 있네!!! 
+								// 거리 구해서 최소값 계속 갱신해줄거야!! 
 					int alt = abs(k - t) - 1;
 					if (alt <= 1) continue;
 					dist[one][two] = min(dist[one][two], alt);
@@ -174,9 +178,8 @@ bool bridge_connection(int one, int two) {
 			}
 		}
 	}
-
+	// 좀이따가 MST 돌릴껀데 그 때 쓰려고 유효한 연결들만 남겨놓음
 	if (dist[one][two] != 987654321 && dist[one][two] > 1) 
-		
 		nodes.push_back(node(one, two, dist[one][two]));
 	
 	return true;
