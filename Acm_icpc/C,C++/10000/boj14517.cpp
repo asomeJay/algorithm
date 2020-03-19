f0208828cef7e51c77f8d1bfbb4462236dcdeb80
asomeJay <museum114@naver.com>
2020-03-19 22:21:21 +0900

    팰린드롬 개수 구하기 : DP
    역발상 : 그 안에것만 생각하지말고 밖으로 뻗어나가는 것도 있음을 생각하라

--- Acm_icpc/C,C++/10000/boj14517.cpp
+++ Acm_icpc/C,C++/10000/boj14517.cpp
@@ -2,11 +2,11 @@
 
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
-bool p[1001][1001];
-int dp[1001][1001];
+int dp[1001];
+
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -15,40 +15,25 @@ int main(void) {
 	cin >> s;
 	int ss = s.size();
 
-	p[0][0] = true;
-	for (int i = 1; i < ss; i++) {
-		p[i][i] = true;
-		if (s[i - 1] == s[i])
-			p[i - 1][i] = true;
-	}
-
-	for (int i = 2; i < ss; i++) {
-		for (int j = 0; j < ss - i; j++) {
-			if (s[j] == s[j + i]) {
-				if (p[j + 1][j + i - 1]) {
-					p[j][j + i] = true;
+	for (int i = 0; i < ss; i++) {
+		for (int j = i; j < ss; j++) {
+			if (s[i] == s[j]) {
+				dp[j]++;
+				for (int k = j + 1; k < ss; k++) {
+					dp[j] += dp[k];
+					dp[j] %= 10007;
 				}
 			}
 		}
 	}
+
 	int cnt = 0;
 	for (int i = 0; i < ss; i++) {
-		for (int j = 0; j <= i; j++) {
-			if (s[j] == s[i]) {
-				cnt++;
-				for (int k = j + 1; k <= i - 1; k++) {
-					for (int m = k; m <= i - 1; m++) {
-						if (p[k][m]) {
-							cnt++;
-							cnt %= 10007;
-						}
-					}
-				}
-			}
-		}
+		cnt += dp[i];
+		cnt %= 10007;
 	}
 
-	cout << cnt % 10007 << '\n';
+	cout << cnt << '\n';
 
 	return 0;
 }
