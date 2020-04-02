38ced9df9d84c42cb85635f18b8d3f1ea0bb5cd9
asomeJay <museum114@naver.com>
2020-04-03 00:49:15 +0900

    :bug: 열쇠 : BFS
    BFS하다가 조건 만족 시 방문 배열 초기화
    공간과 시간은 Trade OFF 된다!

--- Acm_icpc/C,C++/9000/boj9328.cpp
+++ Acm_icpc/C,C++/9000/boj9328.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -11,12 +13,12 @@ const int dr[4] = { -1, 0, 1, 0 };
 const int dc[4] = { 0, 1, 0, -1 };
 vector<vector<char>> map;
 char key[26];
-bool is_visit[101][101];
+bool is_visit[102][102];
 
 void init();
 void input();
 inline bool is_range(int r, int c);
-int open_the_door(int r, int c);
+void bfs(int r, int c);
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
@@ -24,61 +26,102 @@ int main(void) {
 	int tk; cin >> tk;
 
 	for (int i = 0; i < tk; i++) {
-
+		init();
 		input();
-
-
+		bfs(0, 0);
+		cout << paper << '\n';
 	}
 
 	return 0;
 }
 
 inline bool is_range(int r, int c) {
-	return 0 <= r && r < h && 0 <= c && c < w;
+	return 0 <= r && r <= h+1 && 0 <= c && c <= w+1;
 }
 
-int open_the_door(int r, int c) {
-
+void bfs(int r, int c) {
 	queue<pair<int, int>> search;
+
+	is_visit[r][c] = true;
 	search.push({ r,c });
+
 	while (!search.empty()) {
-		int tr = 
+
+		int tr = search.front().first;
+		int tc = search.front().second;
+		search.pop();
+		is_visit[tr][tc] = true;
+
+		if (map[tr][tc] == '$') {
+			paper++;
+			map[tr][tc] = '.';
+		}
+
 		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if (map[nr][nc] == '*' || !is_range(nr, nc)) continue;
-			if (map[nr][nc] >= 'A' && map[nr][nc] <= 'Z') {
-				char c = map[nr][nc];
-				if (!key[c - 'A']) continue;
-			}
+			int nr = tr + dr[i];
+			int nc = tc + dc[i];
+
+			if (!is_range(nr, nc) || is_visit[nr][nc] || map[nr][nc] == '*') continue;
+			if (map[nr][nc] >= 'A' && map[nr][nc] <= 'Z') continue;
+			
 			if (map[nr][nc] >= 'a' && map[nr][nc] <= 'z') {
-				char c = map[nr][nc];
-				key[c - 'a'] = true;
+				if (!key[map[nr][nc] - 'a']) {
+					key[map[nr][nc] - 'a'] = true;
+					memset(is_visit, false, sizeof(is_visit));
+					for (int j = 1; j <= h; j++) {
+						for (int k = 1; k <= w; k++) {
+							if (map[j][k] - 'A' == map[nr][nc] - 'a')
+								map[j][k] = '.';
+						}
+					}
+
+					while (!search.empty())
+						search.pop();
+				}
+				map[nr][nc] = '.';
 			}
-	}
+			search.push({ nr, nc });
 
-	
-
-
+		}
 	}
 }
 
 void init() {
 	paper = 0;
+	memset(key, false, sizeof(key));
+	memset(is_visit, false, sizeof(is_visit));
 }
 
 void input() {
 	cin >> h >> w;
-	map.resize(h);
+	map.resize(h + 2);
 
-	for (int i = 0; i < h; i++)
-		map.resize(w);
+	for (int i = 0; i < h + 2; i++)
+		map[i].resize(w + 2) ;
 
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
+	for (int i = 0; i < h + 2; i++) {
+		map[i][0] = map[i][h + 1] = '.';
+	}
+	for (int i = 0; i < w + 2; i++) {
+		map[0][i] = map[h + 1][i] = '.';
+	}
+
+	for (int i = 1; i <= h; i++) {
+		for (int j = 1; j <= w; j++) {
 			cin >> map[i][j];
 		}
 	}
 
+	string s;
+	cin >> s;
+	if (s[0] == '0') return;
+	for (auto n : s) {
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
+				if (map[i][j] - 'A' == n - 'a')
+					map[i][j] = '.';
+			}
+		}
+	}
 
 }
