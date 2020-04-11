6f32c88df22185eee42dd15919a5fc50036d2350
asomeJay <museum114@naver.com>
2020-04-11 10:37:31 +0900

    :bug: 발전소 : DP + BitMask
    깊이를 하나 덜 세는 것 때문에 계속 틀렸다. Detail is best...

--- Acm_icpc/C,C++/1000/boj1102.cpp
+++ Acm_icpc/C,C++/1000/boj1102.cpp
@@ -3,16 +3,18 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cstring>
 
 #define PP 17
 
 using namespace std;
 
-int N, normal, object, power_plant[1<<16][PP][PP];
+int N, normal, object, power_plant[PP][PP];
+int dp[1 << PP];
 int bit_mask;
 
 void input();
-int dfs(int, int, int, int, int);
+int dfs(int, int);
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
@@ -21,18 +23,19 @@ int main(void) {
 	int result = 987654321;
 
 	input();
-	for (int i = 0; i < N; i++) {
-		if (!(bit_mask & (1 << i))) continue;
-		for (int j = 0; j < N; j++) {
-			if (bit_mask & (1 << j))
-				continue;
-			result = min(result, dfs(normal, power_plant[0][i][j], i, j, (bit_mask | (1 << j))));
-		}
+	
+	if (normal >= object)
+		cout << '0' << '\n';
+	else if (normal == 0)
+		cout << "-1" << '\n';
+
+	else {
+		result = min(result, dfs(normal, bit_mask));
+
+		cout << result;
 	}
 
-	if (result == 987654321)
-		result = -1;
-	cout << result;
+	
 
 	return 0;
 }
@@ -41,7 +44,7 @@ void input() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			cin >> power_plant[0][i][j];
+			cin >> power_plant[i][j];
 		}
 	}
 
@@ -54,19 +57,19 @@ void input() {
 			bit_mask |= (1 << i);
 		}
 	}
-
+	memset(dp, -1, sizeof(dp));
 	cin >> object;
 }
 
-int dfs(int depth, int price, int cur, int tar, int visit) {
-	cout << cur << " " << tar << '\n';
+int dfs(int depth, int visit) {
 	if (depth >= object) {
+		
 		return 0;
 	}
 
-	int& ret = power_plant[visit][cur][tar];
+	int& ret = dp[visit] ;
 
-	if (ret != 0)
+	if (ret != -1)
 		return ret;
 
 	ret = 987654321;
@@ -76,7 +79,7 @@ int dfs(int depth, int price, int cur, int tar, int visit) {
 		for (int obj = 0; obj < N; obj++) {
 			if (src == obj || (visit &(1 << obj))) 
 				continue;
-			ret = min(dfs(depth + 1, price + power_plant[0][src][obj], src, obj, (visit | (1 << obj))), ret);
+			ret = min(ret, power_plant[src][obj] + dfs(depth + 1,  (visit | (1 << obj))));
 		}
 	}
 
