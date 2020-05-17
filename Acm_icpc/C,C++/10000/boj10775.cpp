e897a67e1f1c23f1e4fac56481a3b20b9a2a0fa2
asomeJay <museum114@naver.com>
2020-05-17 23:43:23 +0900

    :rocket: 공항, union_find 이용... 응용!

---
+++ Acm_icpc/C,C++/10000/boj10775.cpp
@@ -0,0 +1,33 @@
+/* 10775 °øÇ× */
+
+#include <iostream>
+
+#define MAX 10001
+
+using namespace std;
+
+int G, P, child;
+int parent[MAX];
+
+int find(int x);
+
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> G >> P;
+
+	for (int i = 0; i < P; i++) {
+		cin >> child;
+		
+	}
+		
+	
+
+
+	return 0;
+}
+
+int find(int x) {
+	if
+}
