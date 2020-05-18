678c407b69a78955435349cbb43f88825cda94bc
asomeJay <museum114@naver.com>
2020-05-18 21:49:02 +0900

    :rocket: 10775 Airport 공항, Union find !

--- Acm_icpc/C,C++/10000/boj10775.cpp
+++ Acm_icpc/C,C++/10000/boj10775.cpp
@@ -2,32 +2,59 @@
 
 #include <iostream>
 
-#define MAX 10001
+#define MAX 100001
 
 using namespace std;
 
 int G, P, child;
 int parent[MAX];
+bool is_used[MAX];
 
 int find(int x);
-
+void merge(int u, int v);
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> G >> P;
 
-	for (int i = 0; i < P; i++) {
+	int i;
+
+	for (i = 0; i <= G; i++) {
+		parent[i] = -1;
+	}
+
+	for (i = 1; i <= P; i++) {
 		cin >> child;
-		
+		if (is_used[0]) continue;
+		int p = find(child);
+		is_used[p] = true;
+
+		merge(p - 1, p);
 	}
 		
-	
+	int sum = 0;
+	for (i = 1; i <= G; i++) {
+		if (is_used[i])
+			sum++;
+	}
 
+	cout << sum << '\n';
 
 	return 0;
 }
 
 int find(int x) {
-	if
+	if (x <= 0) return 0;
+
+	if (parent[x] == -1) return x;
+	else
+		return parent[x] = find(parent[x]);
+}
+
+void merge(int u, int v) {
+	int a = find(u);
+	int b = find(v);
+
+	parent[b] = a;
 }
