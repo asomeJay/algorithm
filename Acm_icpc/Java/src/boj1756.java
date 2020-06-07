f4c821bbbba5fd500154483a5188750b381a8965
asomeJay <museum114@naver.com>
2020-06-07 21:19:27 +0900

    :rocket: 피자굽기, 구현하면 되는거다. 더 멋들어지게 풀려다가 고려하지 못한 사항이 많아서 많이 틀림...
    늘 겸손해야지 ㅠㅠ

---
+++ Acm_icpc/Java/src/boj1756.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class boj1756 {
+    static BufferedWriter bw;
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    static int D, N, end, current_smallest;
+    static int []oven, pizza;
+    static Map<Integer, Integer> check;
+
+    public static void main(String[] args) throws Exception {
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        D = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        oven = new int[300001];
+        pizza = new int[300001];
+
+        st = new StringTokenizer(br.readLine());
+        oven[0] = 2100000000;
+        for(int i = 1; i <= D; i++){
+            oven[i] = Math.min(Integer.parseInt(st.nextToken()), oven[i-1]);
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i <= N;i++){
+            pizza[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int index = 1, i = D;
+        for(i = D; i >0; i--){
+            if(pizza[index] > oven[i]) continue;
+            index++;
+            if(index > N){
+                bw.write(i + "\n");
+                break;
+            }
+        }
+        if(i == 0){
+            bw.write(0 + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
