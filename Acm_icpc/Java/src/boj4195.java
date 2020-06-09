36a31ed9b85584f97eb3650049fffe7ad482923a
asomeJay <museum114@naver.com>
2020-06-09 23:46:22 +0900

    :rocket: 친구 네트워크, HashMap을 적극적으로 활용할 예정

---
+++ Acm_icpc/Java/src/boj4195.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class boj4195 {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
