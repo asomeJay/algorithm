5ba926c9b2659abb796cc8e7659d88eb5046e5cb
asomeJay <museum114@naver.com>
2020-06-13 08:38:55 +0900

    :rocket: 선물, 간단한 이분탐색 문제 여전히 납득은 가지 않는다. A는 길이아닌가... 길이는 소수점이 가능한데...

--- Acm_icpc/Java/src/boj1166.java
+++ Acm_icpc/Java/src/boj1166.java
@@ -7,31 +7,38 @@ public class boj1166 {
     static StringTokenizer st;
 
     static int N;
-    static double L, W, H,ANS;
+    static long L, W, H;
+    static double ANS;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+        bw.write(Double.parseDouble(String.format("%.9f", ANS)) + "\n"); bw.flush(); bw.close();
     }
 
     static void input() throws Exception{
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        L = Float.parseFloat(st.nextToken()) / N;
+       /* L = Float.parseFloat(st.nextToken()) / N;
         W = Float.parseFloat(st.nextToken()) / N;
-        H = Float.parseFloat(st.nextToken()) / N;
+        H = Float.parseFloat(st.nextToken()) / N;*/
     }
 
     static void solve(){
-        long l, r, m;
-        l = 0; r = 1000000000;
-        while(l<=r){
+        double l, r, m;
+        l = 0; r = 1e15;
+        for(int i = 0; i <= 10000; i++){
             m = (l + r) / 2;
-            if(m <= L && m <= W && m <= H){
+            double tot = (long)(L / m) * (long)(W / m) * (long)(H/m);
+            if(tot >= N){
+                l = m ;
                 ANS = m;
-                l = m + 1;
             } else {
-                r = m - 1;
+               r = m;
             }
         }
     }
