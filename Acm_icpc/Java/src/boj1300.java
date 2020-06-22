a0a0a69644fc53ddbbd80da44fe1012c579e14b9
asomeJay <museum114@naver.com>
2020-06-22 13:24:27 +0900

    :rocket: K번째 수, 첫번쨰 Solution은 실패로... 잘 만든 것 같은데

--- Acm_icpc/Java/src/boj1300.java
+++ Acm_icpc/Java/src/boj1300.java
@@ -8,10 +8,13 @@ public class boj1300 {
 
     static int[][] A;
     static int[] B;
-    static int N, K;
+    static int N, K,CNT;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception{
+        input();
+        solve();
+        bw.write(CNT+"\n");
+        bw.flush(); bw.close();
     }
 
     public static void input() throws Exception{
@@ -21,11 +24,29 @@ public class boj1300 {
         st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
 
-        A = new int[N+1][N+1];
-        B = new int[(int)Math.pow(N+1,2)];
+        //A = new int[N+1][N+1];
+        //B = new int[(int)Math.pow(N+1,2)];
     }
 
     public static void solve() throws Exception {
+        int target = 1, sum = 1;
+        while(sum < K){
+           target++;
+           sum += factor(target);
+        }
+        CNT = target;
 
     }
+
+    public static int factor(int n){
+        int cnt = 0, defactor=0;
+        while(defactor <= n){
+            defactor++;
+            if(n / defactor <= N && defactor <= N && n % defactor == 0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
 }
