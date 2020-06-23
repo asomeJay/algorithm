8b709dcf7949df511f4453146f05f56a95e8d41e
asomeJay <museum114@naver.com>
2020-06-24 01:10:30 +0900

    :rocket: 미완의 문제 ㅠㅠㅠ

---
+++ Acm_icpc/codeforce/cofo_652_div2_D.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#define DIV 1000000007
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	int t_case; cin >> t_case;
+	for (int i = 0; i < t_case; i++) {
+		int level_of_rdb; cin >> level_of_rdb;
+		
+
+	}
+
+	return 0;
+}
