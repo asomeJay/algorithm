331d75981a40532467dffe941a2c72d39daad234
김재원 <museum114@naver.com>
2020-07-05 03:05:04 +0900

    :rocket: 아우 피곤해 간단한 문제

---
+++ Acm_icpc/C,C++/1000/boj1789.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+long S;
+
+int main(void) {
+	ios_base:: sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+	cin >> S;
+	long l = 0, r = S;
+	while (l < r) {
+		long mid = (l + r) >> 2;
+
+	}
+	return 0;
+}
