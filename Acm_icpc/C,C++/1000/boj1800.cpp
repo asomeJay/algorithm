248b41b8b310395a4199667f59cc0b79e5822484
김재원 <museum114@naver.com>
2020-07-04 11:00:02 +0900

    :rocket: 인터넷 설치, 이분탐색이랑 다익스트라랑 섞었다. 원하는 게 전선의 수 이기 때문에 전선의 수를 다익스트라에 cost 값으로 넣는다. 그리고 그 cost가 기준점을 넘는 선이 몇 개인지 체크한다.

--- Acm_icpc/C,C++/1000/boj1800.cpp
+++ Acm_icpc/C,C++/1000/boj1800.cpp
@@ -5,13 +5,12 @@
 
 #define INF 2100000000
 #define VISIT 1
+#define p pair<int,int>
 
 using namespace std;
 
-int edge[1001][1001];
+vector<p> edge[1001];
 int dist[1001];
-int weight[1001];
-bool is_visit[1001];
 int N, P, K;
 
 int getSmallest(); 
@@ -22,76 +21,52 @@ int main(void) {
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> N >> P >> K;
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			edge[i][j] = INF;
-		} 
-	}
-	
 	for (int i = 1; i <= P; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-		edge[a][b] = c;
-		edge[b][a] = c;
+		edge[a].push_back({ b,c });
+		edge[b].push_back({ a,c });
 	}
 	int l = 0, r = 1e7, ans = -1;
 
-	while (l < r) {
+	while (l <= r) {
 		int mid = (l + r) / 2;
 		for (int i = 1; i <= N; i++) {
-			is_visit[i] = false;
-			weight[i] = 0;
 			dist[i] = INF;	
 		}
 
 		bool stand = dijkstra(mid);
 		if (stand) { 
-			r = mid;
+			ans = mid;
+			r = mid - 1;
 		}
 		else {
 			l = mid + 1;
 		}
 	}
 
-	cout << r << endl;
+	cout << ans;
 	return 0;
 }
 
-int getSmallest() {
-	int min = INF;
-	int index = -1;
-
-	for (int i = 1; i <= N; i++) {
-		if (dist[i] < min && !is_visit[i]) {
-			min = dist[i];
-			index = i;
-		}
-	}
-
-	return index;
-}
-
 bool dijkstra(int limit) {
-		
+	priority_queue<p> pq; pq.push({ 0,1 });
 	dist[1] = 0;
-	for (int i = 0; i < N; i++) {
-		int current = getSmallest();
-		if (current == -1) break;
-		is_visit[current] = true;
-		for (int j = 1; j <= N; j++) {
-			if (edge[current][j] < dist[j] && !is_visit[j] ){
-						//cout << "current : " << current << " edge : " << j << endl;
-				dist[j] = edge[current][j];
-				if (weight[j] == 0 || weight[j] > weight[current] + (edge[current][j] >= limit))
-					weight[j] = weight[current] + (edge[current][j] > limit);
+	
+	while (!pq.empty()) {
+		int now = pq.top().second;
+		int cost = -1 * pq.top().first;
+		pq.pop();
+
+		if (dist[now] < cost) continue;
+		for (auto i : edge[now]) {
+			int nxt = i.first;
+			int nxtcost = cost + (i.second > limit);
+			if (nxtcost < dist[nxt]) {
+				dist[nxt] = nxtcost;
+				pq.push({ -nxtcost, nxt });
 			}
 		}
 	}
-	/*cout << limit << endl;
-	for (int i = 0; i <= N; i++) {
-		cout << weight[i] << " ";
-	} cout << endl;*/
-	
-	return (weight[N] <= K);
+	return dist[N] <= K;
 }
