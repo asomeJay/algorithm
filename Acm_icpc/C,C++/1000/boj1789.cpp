25ef90442888dd75e75285818ad64b6fca05cf3a
김재원 <museum114@naver.com>
2020-07-05 11:34:54 +0900

    :rocket: 수들의 합, 간단한 이분탐색 문제

--- Acm_icpc/C,C++/1000/boj1789.cpp
+++ Acm_icpc/C,C++/1000/boj1789.cpp
@@ -8,10 +8,19 @@ int main(void) {
 	ios_base:: sync_with_stdio(false);
 	cin.tie(NULL);	cout.tie(NULL);
 	cin >> S;
-	long l = 0, r = S;
-	while (l < r) {
-		long mid = (l + r) >> 2;
+	long l = 0, r = S, ANS = 0;
 
+	while (l <= r) {
+		long mid = (l + r) / 2;
+		long sum = mid * (mid + 1) / 2;
+		if (sum > S) {
+			r = mid - 1;
+		}
+		else {
+			ANS = mid;
+			l = mid + 1;
+		}
 	}
+	cout << ANS << '\n';
 	return 0;
 }
