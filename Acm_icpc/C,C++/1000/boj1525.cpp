6e5504b4141a04502c4ad5caac34178a13482002
김재원 <museum114@naver.com>
2020-07-05 18:51:05 +0900

    :rocket: 퍼즐, 다시 한번 더풀었음

--- Acm_icpc/C,C++/1000/boj1525.cpp
+++ Acm_icpc/C,C++/1000/boj1525.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <map>
 #include <queue>
@@ -6,59 +7,52 @@
 using namespace std;
 
 int dr[4] = { -1,0,1,0 };
-int dc[4] = { 0,1,0,-1 };
+int dc[4] = { 0, 1, 0, -1 };
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	int start = 0;
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			int a; cin >> a;
-			if (a == 0) {
-				a = 9;
-			}
-
-			start = start * 10 + a;
-		}
+	for (int i = 0; i < 9; i++) {
+		int a; cin >> a;
+		if (a == 0) a = 9;
+		start = start * 10 + a;
 	}
 
-	map<int, int> dist;
-	dist[start] = 0;
+	map<string, int> dist;
+	queue<int> bfs;
 
-	queue<int> q;
-	q.push(start);
+	bfs.push(start);
+	dist[to_string(start)] = 0;
 
-	while (!q.empty()) {
-		int now = q.front(); q.pop();
-		string now_string = to_string(now);
-	
-		int pos = now_string.find('9');
-		int r = pos / 3;
-		int c = pos % 3;
+	while (!bfs.empty()) {
+		int now = bfs.front(); bfs.pop();
+		
+		string now_str = to_string(now);
+		int distance = now_str.find('9');
+		int r = distance / 3; int c = distance % 3;
 
 		for (int i = 0; i < 4; i++) {
 			int nr = r + dr[i];
 			int nc = c + dc[i];
-
+			
 			if (nr >= 0 && nr < 3 && nc >= 0 && nc < 3) {
-				string next_string = now_string;
-				swap(next_string[r * 3 + c], next_string[nr * 3 + nc]);
-				if (dist.count(stoi(next_string)) == 0) {
-					dist[stoi(next_string)] = dist[now] + 1;
-					q.push(stoi(next_string));
+				string next_string = now_str;
+				swap(next_string[nr * 3 + nc], next_string[r * 3 + c]);
+				if (dist.count(next_string) == 0) {
+					dist[next_string] = dist[now_str] + 1;
+					bfs.push(stoi(next_string));
 				}
 			}
 		}
 	}
 
-	if (dist.count(123456789) == 0) {
+	if (dist.count("123456789") == 0) {
 		cout << -1 << '\n';
 	}
 	else {
-		cout << dist[123456789] << '\n';
+		cout << dist["123456789"] << '\n';
 	}
 
 	return 0;
