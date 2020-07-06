8e3dee47c3bafea11233f29efcdf936a29694417
김재원 <museum114@naver.com>
2020-07-07 00:51:15 +0900

    :rocket: 합이 0인 네 정수, 두 개로 쪼개는 거 성공. 그리고 두개를 동시에 N을 하는게 아니라 하나는 N 다른 하나는 LogN을 할 수도 있다(이분탐색 활용)

--- Acm_icpc/C,C++/7000/boj7453.cpp
+++ Acm_icpc/C,C++/7000/boj7453.cpp
@@ -29,53 +29,15 @@ int main(void) {
 		}
 	}
 
-	sort(one_two.begin(), one_two.end());
-	sort(three_four.begin(), three_four.end(), comp);
-	
-/*	for (auto i : one_two) {
-		cout << i << " ";
-	} cout << endl;
-	for (auto i : three_four) {
-		cout << i << " ";
-	} cout << endl;*/
+	sort(three_four.begin(), three_four.end());
+	long ans = 0;
+	vector<int>::iterator lo, up;
+	for (int i = 0; i < one_two.size(); i++) {
 
-	int l = 0, r = 0, cnt = 0;
-	while (l != one_two.size() && r != three_four.size()) {
-		int summation = one_two[l] + three_four[r];
-		//cout << one_two[l] << " " << three_four[r] << " " << summation << '\n';
-		if (summation > 0) {
-			r++;
-		}
-		else if (summation < 0) {
-			l ++;
-		}
-		else {
-			int left = 1, right = 1;
-			l++; r++;
-			while (l != one_two.size() && one_two[l] == one_two[l - 1]) {
-				left++;
-			}
-			while (r != three_four.size() && three_four[r] == three_four[r - 1]) {
-				right++;
-			}
-			cnt += (left * right);
-		}
+		int l = lower_bound(three_four.begin(), three_four.end(), -1 * one_two[i]) - three_four.begin();
+		int r = upper_bound(three_four.begin(), three_four.end(), -1 * one_two[i]) - three_four.begin();
+		ans += (r - l);
 	}
-
-	while (l != one_two.size()) {
-		if (one_two[l] == 0) {
-			cnt++;
-		}
-		l++;
-	}
-	
-	while (r != three_four.size()) {
-		if (three_four[r] == 0) {
-			cnt++;
-		}
-		r++;
-	}
-
-	cout << cnt << '\n';
+	cout << ans << '\n';
 	return 0;
 }
