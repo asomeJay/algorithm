2ea5bf03522ec95771557a63a07ae1b1e29d3033
asomejay <museum114@naver.com>
2020-07-09 12:46:15 +0900

    :rocket: 합, 쾌감쩐다... 투포인터같은 문제이다. leftmost digit부터 할 생각이 있으면 rightmost digit도 떠올려보자. 1 ~ N까지 합 구하기 이거 잘 쓰일것 같은데 풀이를 기억할 필요가 있다. 각 digit마다 따로 떼어내서 생각하니까 훨씬 잘풀린다

--- Acm_icpc/C,C++/1000/boj1081.cpp
+++ Acm_icpc/C,C++/1000/boj1081.cpp
@@ -2,10 +2,37 @@
 #include <string>
 #include <algorithm>
 
+#define ll long long int
+
 using namespace std;
 
 int L, U;
-long number[10];
+
+ll sum(ll n) {
+	ll number[10] = { 0, };
+	ll s = 1, result = 0;
+	while (s <= n) {
+		ll l = n / (s * 10);
+		ll r = n % (s * 10);
+	
+		for (int i = 0; i < 10; i++) number[i] += l * s;
+		for (int i = 0; i < r / s; i++) number[i] += s;
+		number[r / s] += 1+ r % s;
+		
+		/*cout << "S : " << s << endl;
+		for (int i = 1; i < 10; i++) {
+			cout << number[i] << " ";
+		}cout << endl;*/
+		s *= 10;
+	}
+
+	for (int i = 1; i < 10; i++) {
+		result += i * number[i];
+		//cout << number[i] << " ";
+	} //cout << endl;
+	//cout << result << endl;
+	return result;
+}
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
@@ -13,21 +40,8 @@ int main(void) {
 
 	cin >> L >> U;
 
-	int cur = 0;
-	string s_L = to_string(L);
-	string s_U = to_string(U);
+	cout << sum(U) - sum(L-1) << '\n';
 
-	while (cur != s_L.size()){
-		if (s_L[cur] == s_U[cur]) {
-			string t_L = s_L.substr(cur);
-			string t_U = s_U.substr(cur);
-			number[s_L[cur] - '0'] += (stoi(t_L) - stoi(t_U));
-		}
-		else {
-
-		}
-		
-	}
 	
 	return 0;
 }
