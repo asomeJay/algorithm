9cef44fdf894253438f02c2efac256cef80767e4
asomejay <museum114@naver.com>
2020-07-12 21:41:39 +0900

    :rocket: 알 수 없는 문장, DP문제인데 엄청 고생헀다... 하 완전 뿌듯해 다 조졌다!!!

--- Acm_icpc/C,C++/1000/boj1099.cpp
+++ Acm_icpc/C,C++/1000/boj1099.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 
+#define INT_MAX 1000000000
 using namespace std;
 
 string main_str;
@@ -19,8 +20,12 @@ int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> main_str >> N;
-	dp.resize(N);
+	main_str.push_back('0');
+	string t; cin >> t; main_str += t;
+
+	cin >> N;
+
+	dp.resize(main_str.size() + 1, 0);
 
 	for (int i = 0; i < N; i++) {
 		string temp;
@@ -29,23 +34,54 @@ int main(void) {
 	}
 
 	solve();
-	cout << dp[N] << '\n';
+	int ANS = ((dp[main_str.size() - 1] == INT_MAX / 2) ? -1 : dp[main_str.size() - 1]);
+	cout << ANS;
 
 	return 0;
 }
 
 void solve() {
 
-	for (int i = 1; i <= N; i++) {
+	for (int i = 1; i < main_str.length(); i++) {
 		// Determine DP		
-
-		int mini = 0;
-		for (int j = 1; j <= i; j++) {
-			mini = min(mini, dp[i - j] + cost(j, i-j));
+		// DP[i] = DP[i-1] + S[1] ...
+		int mini = INT_MAX / 2;
+//		cout << i << endl;
+		for (int j = 0; j < i; j++){ 
+			mini = min(mini, dp[j] + cost(j+1, i - j));
 		}
+		dp[i] = mini;
 	}
 }
 
-int cost(int idx) {
-		
+int cost(int start_idx, int range) {
+//	cout << start_idx << " " << range << endl;
+	int answer = INT_MAX/2, idx, j;
+
+	for (auto word : words[range]) { // word를 받음
+		int dif = 0;
+		string temp = word; // 얘는 지울거라 복사함 
+		//cout << word << endl;
+		for (j = start_idx; j < start_idx + range; j++) {
+			if ((idx = temp.find(main_str[j])) == string::npos) {
+				break;
+			}
+			else {
+				// 순서로 Cost를 써야하는뎅 
+				// 좋아 간드아~~
+				temp[idx] = '0';
+			}
+		}
+
+		if (j == start_idx + range) { // 매칭되는 애가 있음 이제 그 비용을 구해야지
+			for (int k = 0; k < word.size(); k++) {
+				if (word[k] != main_str[start_idx + k]) {
+					dif++;
+				}
+			}
+			answer = min(dif, answer);
+		}	
+	}
+	
+	return answer;
 }
