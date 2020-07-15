fef65eaf97afcde7571ff1191de43ca23ed1562c
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-07-15 19:22:09 +0900

    :rocket: 램프, 무조건 완전탐색이 아니라, 우선 끄고 켜는 걸로 해결할 수 있는 수준인가, 그리고 하나를 건드리면 다 건드리는 꼴이라서 일치하는 것을 찾으면 된다.

--- Acm_icpc/C,C++/1000/boj1034.cpp
+++ Acm_icpc/C,C++/1000/boj1034.cpp
@@ -3,61 +3,60 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 int R, C, K, ANS;
-bool lamp[51][51];
+string lamp[51];
+//int lamp[51][51];
+int line[51];
 
-void dfs(int, bool[51][51], int);
+void dfs(int, int);
+void input();
+void solve();
+void push_the_switch(int column);
 
 int main(void) {
-	scanf("%d %d", &R, &C);
-	getchar();
-
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			scanf("%c", &lamp[i][j]);
-			lamp[i][j] -= '0';
-		}
-		getchar();
-	}
-
-	scanf("%d", &K);
-	dfs(0, lamp, K);
-	cout << ANS;
+	input();
+	solve();
 
 	return 0;
 }
 
-void dfs(int c, bool before_lamp[51][51], int k) {
-	if (c == C) { // 끝까지 도착함.
-		int i, j, cur_max = 0;
-		for (i = 0; i < R; i++) {
-			for (j = 1; j < C; j++) {
-				if (before_lamp[i][j - 1] != before_lamp[i][j] || before_lamp[i][j] != 1)
-					break;
-			}
-			if (j == C) cur_max++;
-		}
-		ANS = max(ANS, cur_max);
-		return;
-	}
-
-	bool cur_lamp[51][51];
-
+void solve() {
 	for (int i = 0; i < R; i++) {
+		int cnt = 0;
+		int same = 0;
+
 		for (int j = 0; j < C; j++) {
-			cur_lamp[i][j] = before_lamp[i][j];
+			if (lamp[i][j] == '0') {
+				cnt++;
+			}
+		}
+
+		if (cnt <= K && (K - cnt) % 2 == 0) {
+			for (int k = i; k < R; k++) {
+				if (lamp[i] == lamp[k]) {
+					same++;
+				}
+			}
+
+			ANS = max(ANS, same);
 		}
 	}
 
-	dfs(c + 1, cur_lamp, k); // 이번에 불을 안 켤거임.
+	printf("%d\n", ANS);
+}
+
+void input() {
+
+	scanf("%d %d", &R, &C);
+	getchar();
+
 	for (int i = 0; i < R; i++) {
-		cur_lamp[i][c] = (cur_lamp[i][c] == 0 ? 1 : 0);
+		cin >> lamp[i];
 	}
 
-	if (k != 0) {
-		dfs(c + 1, cur_lamp, k - 1);
-	}	
+	scanf("%d", &K);
 }
