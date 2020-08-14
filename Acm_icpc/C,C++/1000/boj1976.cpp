dab761145f7879250a8a1b47b44768c0fcafb034
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-08-14 11:05:41 +0900

    :rocket: 여행 가자 Disjoin SEt ver

--- Acm_icpc/C,C++/1000/boj1976.cpp
+++ Acm_icpc/C,C++/1000/boj1976.cpp
@@ -8,77 +8,52 @@ using namespace std;
 int n_of_city, n_of_travel;
 bool edge[200 + 1][200 + 1];
 int travel[1000 + 1];
+int parent[200 + 1];
 
 void input();
-void solve();
+
+int find(int x) {
+	if (x == parent[x]) return x;
+	return parent[x] = find(parent[x]);
+}
+
+void unions(int x, int y) {
+	int px = find(x);
+	int py = find(y);
+	parent[px] = py;
+}
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
 	input();
-	solve();
 
 	return 0;
 }
 
 void input() {
 	cin >> n_of_city >> n_of_travel;
+
 	for (int i = 1; i <= n_of_city; i++) {
+		parent[i] = i;
+
 		for (int j = 1; j <= n_of_city; j++) {
 			cin >> edge[i][j];
-		}
-	}
-
-	for (int i = 1; i <= n_of_city; i++) {
-		edge[i][i] = true;
-	}
-
-	for (int i = 1; i <= n_of_travel; i++) {
-		cin >> travel[i];
-	}
-}
-
-void bfs(int idx) {
-	queue<int> q;
-	q.push(idx);
-	bool is_visit[200 + 1] = { false, };
-	is_visit[idx] = true;
-	while (!q.empty()) {
-		int curr = q.front();
-		q.pop();
-
-		for (int i = 1; i <= n_of_city; i++) {
-			if (edge[curr][i] == true && is_visit[i] == false) {
-				edge[idx][i] = true;
-				edge[i][idx] = true;
-
-				is_visit[i] = true;
-
-				q.push(i);
+			if (edge[i][j] == true) { // 연결한다. 
+				unions(i, j);
 			}
 		}
 	}
-}
 
-void solve() {
-	for (int i = 1; i <= n_of_city; i++) {
-		bfs(i);
-	}
-
-/*	for (int i = 1; i <= n_of_city; i++) {
-		for (int j = 1; j <= n_of_city; j++) {
-			cout << edge[i][j] << ' ';
-		}cout << endl;
-	}*/
-
-	for (int i = 1; i <= n_of_travel-1; i++) {
-		if (edge[travel[i]][travel[i + 1]] == false) {
-			cout << "NO" << '\n';
+	cin >> travel[1];
+	for (int i = 2; i <= n_of_travel; i++) {
+		cin >> travel[i];
+		if (parent[travel[i - 1]] != parent[travel[i]]) {
+			cout << "NO\n";
 			return;
 		}
 	}
-
 	cout << "YES\n";
 	return;
 }
