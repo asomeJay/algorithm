552aa34d31d5eabc56ec9a08a944306f234fe467
asomeJay <45970468+asomeJay@users.noreply.github.com>
2020-10-05 13:00:09 +0900

    :rocket: 백준 1670 정상회담 2, DP

--- Acm_icpc/Java/src/algorithm/src/algorithm/boj1670.java
+++ Acm_icpc/Java/src/algorithm/src/algorithm/boj1670.java
@@ -3,29 +3,31 @@ import java.util.StringTokenizer;
 
 public class boj1670 {
 
-    static int N;
+    static long[] dp = new long[10010];
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N;
         N = Integer.parseInt(st.nextToken());
-
-        bw.write(bt(-1) + "\n") ;
+        dps(N);
+        bw.write(dp[N] + "\n");
         bw.flush();
     }
 
-    static int bt(int index){
-        if(index == (2/N)){
-            return 1;
+    static void dps(int N) {
+        dp[0] = 1;
+        dp[2] = 1;
+        for(int i = 4; i <= N; i += 2){
+            long temporalSum = 0;
+            for(int j = i-2; j >= 0; j -=2){
+                temporalSum += ((dp[j] * dp[i - 2 -j]) % 987654321);
+                temporalSum %= 987654321;
+            }
+            dp[i] = temporalSum;
+
         }
-
-        if(index > (2/N)) return 0;
-        int ret = 0;
-        ret += bt(index + 1) % 987654321;
-        ret += bt(index + 2) % 987654321;
-
-        return ret % 987654321;
     }
 }
